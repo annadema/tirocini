ccecbaa81d4b1413bb91a81a62a55bfc530230a1
aladar <me@gmail.com>
2023-06-02 14:56:37 +0200

    Ho inserito la Validazione Ho installato i package: class-vlaidator e class-transormer Ho creato la cartella Users/dtos Ho creato il file create-user.dto.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 @Controller('users')
 export class UsersController {
   @Get()
-  listAllUsers() {
+  listUsers() {
     console.log('ok');
+    return 'ok';
+  }
+  @Post()
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getUser(@Param('id') id: string) {
+    console.log(id);
   }
 }
