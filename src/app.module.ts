91903bdefad93204f791186d792ef37d6e1b09d3
Annamaria De Martino <annamaria.demartino@gmail.com>
2023-06-02 09:51:48 +0200

    Configurata connessione con database tirocini su mysql server parte correttamente installato mysql2 al posto di mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UsersDataModule } from './users_data/users_data.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, UsersDataModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'tirocini',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    UsersDataModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
