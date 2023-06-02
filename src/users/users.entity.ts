5563bbb2bd5dc0060e074832ce2b11662c0808f3
aladar <me@gmail.com>
2023-06-02 16:08:02 +0200

    Added all columns of Users table in the Entity Users TEsted that the Users table on DB dinamically add/remove columns

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -4,8 +4,27 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 export class Users {
   @PrimaryGeneratedColumn()
   id: number;
+  custom_id: number;
   @Column()
   username: string;
   @Column()
   password: string;
+  @Column()
+  time_zone: string;
+  @Column()
+  enabled: number; //capire come definire un intero più piccolo
+  @Column()
+  latest_access: string; //capire come definire una data
+  @Column()
+  activation_token: string;
+  @Column()
+  activation_token_date: string; //idem data
+  @Column()
+  reset_password_token: string;
+  @Column()
+  reset_password_token_date: string; //idem data
+  @Column()
+  tenantOprationalOfficeId: number;
+  @Column()
+  roleId: number;
 }
